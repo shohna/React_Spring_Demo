d252f1b5e20bd4cc42bd9e617ab268bf62a4dec9
shohna <shohnakanchan14@gmail.com>
2020-05-10 12:23:25 +0530

    My first commit

---
+++ MyReactApp/src/main/java/com/shohna/MyReactApp/MyReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.shohna.MyReactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReactAppApplication.class, args);
+	}
+
+}
---
+++ MyReactApp/src/test/java/com/shohna/MyReactApp/MyReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shohna.MyReactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
