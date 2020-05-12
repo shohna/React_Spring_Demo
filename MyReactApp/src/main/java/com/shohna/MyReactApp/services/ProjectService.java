f23e2273e104a57b11433ed24f3d2f68f5e148ed
shohna <shohnakanchan14@gmail.com>
2020-05-12 23:32:37 +0530

    Added frontend module

--- MyReactApp/src/main/java/com/shohna/MyReactApp/services/ProjectService.java
+++ MyReactApp/src/main/java/com/shohna/MyReactApp/services/ProjectService.java
@@ -43,5 +43,6 @@ public class ProjectService {
 		}
 		projectRepository.delete(project);
 	}
-
+	
+	
 }
---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/addProject" component={AddProject} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
